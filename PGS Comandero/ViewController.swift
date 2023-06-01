f3572d27caf17c4b9a879245902f94baaa924f7b
Daniel Flores <danielflores@pargolfsolutions.com>
2023-06-01 12:15:42 -0500

    Comandero nativo ios

---
+++ PGS Comandero/ViewController.swift
@@ -0,0 +1,325 @@
+import UIKit
+import WebKit
+
+var webView: WKWebView! = nil
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var loadingView: UIView!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var connectionProblemView: UIImageView!
+    @IBOutlet weak var webviewView: UIView!
+    @IBOutlet weak var splashBkgView: UIView!
+    var toolbarView: UIToolbar!
+    
+    var htmlIsLoaded = false
+    
+    private var themeObservation: NSKeyValueObservation?
+    var currentWebViewTheme: UIUserInterfaceStyle = .unspecified
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        if #available(iOS 13, *), overrideStatusBar{
+            if #available(iOS 15, *) {
+                return .default
+            } else {
+                return statusBarTheme == "dark" ? .lightContent : .darkContent
+            }
+        }
+        return .default
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initWebView()
+        initToolbarView()
+        loadRootUrl()
+                
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification , object: nil)
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        PGSComandero.webView.setNeedsLayout()
+    }
+    
+    func initWebView() {
+        PGSComandero.webView = createWebView(container: webviewView, WKSMH: self, WKND: self, NSO: self, VC: self)
+        webviewView.addSubview(PGSComandero.webView);
+        PGSComandero.webView.uiDelegate = self;
+        PGSComandero.webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        // PGSComandero.webView.configuration.userContentController.add(self, name: "getRequest")
+        
+        if #available(iOS 15.0, *), adaptiveUIStyle {
+            themeObservation = PGSComandero.webView.observe(\.underPageBackgroundColor) { [unowned self] webView, _ in
+                currentWebViewTheme = PGSComandero.webView.underPageBackgroundColor.isLight() ?? true ? .light : .dark
+                self.overrideUIStyle()
+            }
+        }
+    }
+    
+    func createToolbarView() -> UIToolbar{
+        let statusBarHeight = getStatusBarHeight()
+        let toolbarView = UIToolbar(frame: CGRect(x: 0, y: 0, width: webviewView.frame.width, height: 0))
+        toolbarView.sizeToFit()
+        toolbarView.frame = CGRect(x: 0, y: 0, width: webviewView.frame.width, height: toolbarView.frame.height + statusBarHeight)
+//        toolbarView.autoresizingMask = [.flexibleTopMargin, .flexibleRightMargin, .flexibleWidth]
+        
+        let flex = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let close = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(loadRootUrl))
+        toolbarView.setItems([close,flex], animated: true)
+        
+        toolbarView.isHidden = true
+        
+        return toolbarView
+    }
+
+    func getStatusBarHeight() -> CGFloat {
+        let winScene = UIApplication.shared.connectedScenes.first
+        let windowScene = winScene as! UIWindowScene
+        var statusBarHeight = windowScene.statusBarManager?.statusBarFrame.height ?? 60
+        
+        #if targetEnvironment(macCatalyst)
+        if (statusBarHeight == 0) {
+            statusBarHeight = 30
+        }
+        #endif
+        
+        return statusBarHeight;
+    }
+    
+    func initToolbarView() {
+        toolbarView =  createToolbarView()        
+        webviewView.addSubview(toolbarView)
+
+        // Set the top of the splashBkgView to the bottom of the status bar.
+//        let statusBarHeight = getStatusBarHeight()
+//        let splashBkgFrame = self.splashBkgView.frame
+//        self.splashBkgView.frame = CGRect(x: splashBkgFrame.minX, y: statusBarHeight, width: splashBkgFrame.width, height: splashBkgFrame.height)
+    }
+    
+    func overrideUIStyle(toDefault: Bool = false) {
+        if #available(iOS 15.0, *), adaptiveUIStyle {
+            if (((htmlIsLoaded && !PGSComandero.webView.isHidden) || toDefault) && self.currentWebViewTheme != .unspecified) {
+                UIApplication
+                    .shared
+                    .connectedScenes
+                    .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+                    .first { $0.isKeyWindow }?.overrideUserInterfaceStyle = toDefault ? .unspecified : self.currentWebViewTheme;
+            }
+        }
+    }
+    
+    @objc func loadRootUrl() {
+        // Was the app launched via a universal link? If so, navigate to that.
+        // Otherwise, see if we were launched via shortcut and nav to that.
+        // If neither, just nav to the main PWA URL.
+        let launchUrl = SceneDelegate.universalLinkToLaunch ?? SceneDelegate.shortcutLinkToLaunch ?? rootUrl;
+        print("ESTA ES LA URL: \(launchUrl)")
+        PGSComandero.webView.load(URLRequest(url: rootUrl))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
+        htmlIsLoaded = true;
+        
+        self.setProgress(1.0, true);
+        self.animateConnectionProblem(false);
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            PGSComandero.webView.isHidden = false;
+            self.loadingView.isHidden = true;
+           
+            self.setProgress(0.0, false);
+            
+            self.overrideUIStyle()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        htmlIsLoaded = false;
+        
+        if (error as NSError)._code != (-999) {
+            self.overrideUIStyle(toDefault: true);
+            
+            webView.isHidden = true;
+            loadingView.isHidden = false;
+            animateConnectionProblem(true);
+            
+            setProgress(0.05, true);
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.setProgress(0.1, true);
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.loadRootUrl();
+                }
+            }
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+
+        if (keyPath == #keyPath(WKWebView.estimatedProgress) &&
+                PGSComandero.webView.isLoading &&
+                !self.loadingView.isHidden &&
+                !self.htmlIsLoaded) {
+                    var progress = Float(PGSComandero.webView.estimatedProgress);
+                    
+                    if (progress >= 0.8) { progress = 1.0; };
+                    if (progress >= 0.3) { self.animateConnectionProblem(false); }
+                    
+                    self.setProgress(progress, true);
+            
+        }
+    }
+    
+    func setProgress(_ progress: Float, _ animated: Bool) {
+        self.progressView.setProgress(progress, animated: animated);
+    }
+    
+    func animateConnectionProblem(_ show: Bool) {
+        if (show) {
+            self.connectionProblemView.isHidden = false;
+            self.connectionProblemView.alpha = 0
+            UIView.animate(withDuration: 0.7, delay: 0, options: [.repeat, .autoreverse], animations: {
+                self.connectionProblemView.alpha = 1
+            })
+        }
+        else {
+            UIView.animate(withDuration: 0.3, delay: 0, options: [], animations: {
+                self.connectionProblemView.alpha = 0 // Here you will get the animation you want
+            }, completion: { _ in
+                self.connectionProblemView.isHidden = true;
+                self.connectionProblemView.layer.removeAllAnimations();
+            })
+        }
+    }
+        
+    deinit {
+        PGSComandero.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+}
+
+extension ViewController: WKScriptMessageHandler {
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+      print(message.name)
+        if message.name == "print" {
+            printView(webView: PGSComandero.webView)
+        }
+        if message.name == "push-subscribe" {
+            handleSubscribeTouch(message: message)
+        }
+        if message.name == "push-permission-request" {
+            handlePushPermission()
+        }
+        if message.name == "push-permission-state" {
+            handlePushState()
+        }
+      if (message.name == "getRequest") {
+          print(message.body)
+          if let objStr = message.body as? String {
+              
+              let data: Data = objStr.data(using: .utf8)!
+              do {
+                  let jsObj = try JSONSerialization.jsonObject(with: data, options: .init(rawValue: 0))
+                  print(jsObj)
+                  if let config = jsObj as? Dictionary<String, Any> {
+                      let url = URL(string: config["url"] as! String)
+                      var request = URLRequest(url: url!)
+
+                      request.httpMethod = config["method"] as? String
+
+                      if (request.httpMethod != "GET") {
+                          let bodyData = try? JSONSerialization.data(
+                            withJSONObject: config["data"]!,
+                              options: []
+                          )
+                          request.httpBody = bodyData
+                          
+                          request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                      }
+
+                      let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                          if let data = data {
+                              print("withresponse", response ?? "NO RESPONSE")
+                              DispatchQueue.main.async {
+                                  PGSComandero.webView
+                                      .evaluateJavaScript(
+                                        "javascript:setResponse('success', '\(data)', '\(String(describing: config["id"]))')",
+                                        completionHandler: {
+                                            (data, error) in
+                                            if let data = data {
+                                                print("Received: ", data)
+                                            }
+                                            if let error = error {
+                                                print(error)
+                                            }
+                                        }
+                                      )
+                              }
+                          } else if let error = error {
+                              print("witherror")
+                              DispatchQueue.main.async {
+                                  PGSComandero.webView
+                                      .evaluateJavaScript(
+                                        "javascript:setResponse('error', '\(error)', '\(String(describing: config["id"]))')",
+                                        completionHandler: {
+                                            (data, error) in
+                                            if let data = data {
+                                                print("Received: ", data)
+                                            }
+                                            if let error = error {
+                                                print(error)
+                                            }
+                                        }
+                                      )
+                              }
+                          }
+                      }
+                      
+                      task.resume()
+                  } else {
+                      print("no config")
+                  }
+              } catch {
+                  print("error in config")
+                  let nserror = error as NSError
+                  
+                  PGSComandero.webView
+                      .evaluateJavaScript(
+                        "javascript:setResponse('error', '\(nserror.description)')",
+                        completionHandler: {
+                            (data, error) in
+                            if let data = data {
+                                print("Received: ", data)
+                            }
+                            if let error = error {
+                                print(error)
+                            }
+                        }
+                      )
+              }
+          } else {
+              print("NO MESSAGE BODY PTM")
+          }
+      }
+  }
+}
+
+extension UIColor {
+    // Check if the color is light or dark, as defined by the injected lightness threshold.
+    // Some people report that 0.7 is best. I suggest to find out for yourself.
+    // A nil value is returned if the lightness couldn't be determined.
+    func isLight(threshold: Float = 0.5) -> Bool? {
+        let originalCGColor = self.cgColor
+
+        // Now we need to convert it to the RGB colorspace. UIColor.white / UIColor.black are greyscale and not RGB.
+        // If you don't do this then you will crash when accessing components index 2 below when evaluating greyscale colors.
+        let RGBCGColor = originalCGColor.converted(to: CGColorSpaceCreateDeviceRGB(), intent: .defaultIntent, options: nil)
+        guard let components = RGBCGColor?.components else {
+            return nil
+        }
+        guard components.count >= 3 else {
+            return nil
+        }
+
+        let brightness = Float(((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000)
+        return (brightness > threshold)
+    }
+}
